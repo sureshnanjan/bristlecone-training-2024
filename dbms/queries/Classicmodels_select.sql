ffa1a8d5272b4b8b74cd219f9539d67a09951604
Meghana.MS <meghanasuman0109@gmail.com>
2024-02-27 12:33:46 +0530

    Classic models commits

---
+++ dbms/queries/Classicmodels_select.sql
@@ -0,0 +1,5 @@
+select * from customers;
+select * from employees;
+select * from offices;
+select * from orders;
+select * from payments;
---
+++ dbms/queries/order_where_like.sql
@@ -0,0 +1,8 @@
+select * from payments order by paymentDate asc;
+SELECT * FROM offices
+ORDER BY city DESC;
+
+SELECT *
+FROM offices
+WHERE  city LIKE 'S%';
+
