019ceefb8cc9dbe88498c0a6e0dcc88e3b181336
sureshnanjan <sureshnanjan@outlook.com>
2024-03-01 14:48:31 +0530

    GCP Commandline

---
+++ gcp/Administering-GCP.ps1
@@ -0,0 +1,33 @@
+﻿# gcloud init
+# cloud storage commands
+$currentProjBuckets = Get-GcsBucket
+$specificProjBuckets = Get-GcsBucket -Project my-project-1
+$bucket = Get-GcsBucket -Name my-bucket-name
+
+Get-PSProvider
+
+# Project commands
+Get-GcpProject
+
+Get-Help Get-GcpProject -Examples
+
+# Compute Instances commands
+Get-GceInstance -Project bristlecone-gcp |
+    Sort Status |
+    Format-Table Name, Status -GroupBy Status
+Get-Command -Module GoogleCloud -Verb get
+
+# IAM commands
+Get-GcIamPolicyBinding | select -Property Members
+
+# SQL Commands
+Get-GcSqlInstance | select -ExpandProperty Settings
+GcSqlSettingConfig
+$(Get-GcSqlInstance).Settings
+# cloud storage commands
+# Navigate to Cloud Storage
+cd gs:\
+# Show the available buckets
+ls
+# Create a new bucket
+mkdir my-new-bucket
